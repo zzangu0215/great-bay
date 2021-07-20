24d138a71afe5d23eb26435dbcb99376a3ee7e5e
Junny <tajo0215@gmail.com>
2021-07-19 20:10:49 -0700

    minor changes

--- greatBay.js
+++ greatBay.js
@@ -6,7 +6,7 @@ const connection = mysql.createConnection({
     // Your username
     user: "root",
     // Your password
-    password: "password",
+    password: "Tvmflaldj09!",
     database: "greatBay_DB"
 });
 
